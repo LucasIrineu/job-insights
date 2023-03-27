ba5e2257531fb83c78494dc81869a3dd22cf88ec
Lucas Irineu <lucasirineu119@gmail.com>
2023-03-27 15:49:43 -0300

    requsitos 1, 2 e 3

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,40 +1,22 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path, encoding='utf-8') as file:
+        job_reader = csv.DictReader(file)
+        result = [row for row in job_reader]
+        return result
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    data = read(path)
+    unique_jobs = set()
+    for job in data:
+        unique_jobs.add(job["job_type"])
+    return unique_jobs
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
